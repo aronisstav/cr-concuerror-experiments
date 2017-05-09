ff16ddd24ca7b2f395d075faebab0e6d049131a4
Stavros Aronis <aronisstav@gmail.com>
2017-05-09 14:26:24 +0200

    Convert layout server to an ETS table

--- src/layout_server.erl
+++ src/layout_server.erl
@@ -1,64 +1,27 @@
 
 -module(layout_server).
 
--behaviour(gen_server).
-
 %% API
 -export([start_link/3, read/1, write/3, stop/1]).
 
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
-
--record(state, {
-          name   :: atom(),
-          epoch  :: non_neg_integer(),
-          layout :: term()
-         }).
-
 start_link(Name, InitialEpoch, InitialLayout) when InitialEpoch >= 0 ->
-    gen_server:start_link({local, Name},
-                          ?MODULE, [Name, InitialEpoch, InitialLayout], []).
+  Name = ets:new(Name, [named_table, public]),
+  true = ets:insert(Name, [{epoch,InitialEpoch}, {layout,InitialLayout}]),
+  {ok, self()}.
 
 read(Name) ->
-    gen_server:call(Name, read, infinity).
+  [{epoch, Epoch}] = ets:lookup(Name, epoch),
+  [{layout, Layout}] = ets:lookup(Name, layout),
+  {ok, Epoch, Layout}.
 
-write(Name, Epoch, Layout) ->
-    gen_server:call(Name, {write, Epoch, Layout}, infinity).
+write(Name, NewEpoch, NewLayout) ->
+  [{epoch, Epoch}] = ets:lookup(Name, epoch),
+  if is_integer(NewEpoch), NewEpoch > Epoch ->
+      ets:insert(Name, [{epoch,NewEpoch}, {layout,NewLayout}]),
+      ok;
+     true ->
+      bad_epoch
+  end.
 
 stop(Name) ->
-    gen_server:call(Name, stop, infinity).
-
-%%%%%%%%%%%%%%%%%%%%%%
-
-init([Name, InitialEpoch, InitialLayout]) ->
-    {ok, #state{name=Name,
-                epoch=InitialEpoch,
-                layout=InitialLayout}}.
-
-handle_call(read, _From,
-            #state{epoch=Epoch, layout=Layout} = S) ->
-    {reply, {ok, Epoch, Layout}, S};
-handle_call({write, NewEpoch, NewLayout}, _From,
-            #state{epoch=Epoch} = S) ->
-    if is_integer(NewEpoch), NewEpoch > Epoch ->
-            {reply, ok, S#state{epoch=NewEpoch, layout=NewLayout}};
-       true ->
-            {reply, bad_epoch, S}
-    end;
-handle_call(stop, _From, S) ->
-    {stop, normal, ok, S}.
-
-handle_cast(_Msg, S) ->
-    {noreply, S}.
-
-handle_info(_Info, S) ->
-    {noreply, S}.
-
-terminate(_Reason, _S) ->
-    ok.
-
-code_change(_OldVsn, S, _Extra) ->
-    {ok, S}.
-
-%%%%%%%%%%%%%%%%%%%%%%
+  ets:delete(Name).
